c2c9c105155562d28ac8dbedf1621dad96aad104
vikas <vitekarvikas55@gmail.com>
2024-01-31 23:41:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+    // MOBILE MENU NAVBAR START
+
+    function showSidebar(){
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = "flex";
+        console.log("show")
+        
+    }    
+
+    
+    function closeSidebar(){
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.display = "none";
+        console.log("close")
+        
+    }
+
+     // MOBILE MENU NAVBAR END
+      
